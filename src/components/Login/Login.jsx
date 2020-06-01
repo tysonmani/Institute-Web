3fbacbfa2f411b63ac3e71b4a3dc5c193a5fb318
t.manikanta reddy <thikkavarapureddy@ninjacart.com>
2020-06-01 13:29:46 +0530

    LoginChanges

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,13 +6,15 @@ import { Modal, ModalHeader, ModalFooter, ModalBody } from 'reactstrap';
 import Button from '@material-ui/core/Button';
 import PersonIcon from '@material-ui/icons/Person';
 import { baseUrl } from '../ServerUrls';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 
 function Login() {
-
-    const [MobileNumber, setMobileNumber] = useState('');
-    const [OTP, setOTP] = useState('');
-    const [ErrorStatus, setErrorStatus] = useState(null);
-    const [isModalOpen, setisModalOpen] = useState(false);
 
     let history = useHistory();
 
@@ -20,23 +22,56 @@ function Login() {
         console.log(history, history.location);
     }, []);
 
+    const [MobileNumber, setMobileNumber] = useState('');
+    const [OTP, setOTP] = useState('');
+    // const [ErrorStatus, setErrorStatus] = useState(null);
+    const [isModalOpen, setisModalOpen] = useState(false);
+    const [MessageHandler, setMessageHandler] = useState({ message: '', success: true });
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
     const mySubmitHandler = () => {
         console.log(MobileNumber, OTP);
-        const logindetails = { "mobileNumber": MobileNumber };
-        axios.post(baseUrl + '/institute/user/login', logindetails)
-            .then(response => {
-                console.log(response, history);
-                toggleModal();
-            })
-            .catch(error => {
-                console.log(error, error.response, error.message, error.request);
-                setErrorStatus(error.response.data);
-            })
+        // const logindetails = { "mobileNumber": MobileNumber };
+        // axios.post(baseUrl + '/institute/user/login', logindetails, {headers: {'Authorization':'Basic bmFyYXNpbW1hbjoxMjM0NTY4OQ=='}})
+        //     .then(response => {
+        //         console.log(response, history);
+        //         toggleModal();
+        //     })
+        //     .catch(error => {
+        //         console.log(error, error.response, error.message, error.request);
+        //         setErrorStatus(error.response.data);
+        //     })
         toggleModal();
     }
 
     const getOtpHandler = () => {
         console.log(MobileNumber);
+        const logindetails = { "mobileNumber": MobileNumber };
+        axios.post(baseUrl + '/institute/user/login', logindetails, { headers: { 'Authorization': 'Basic bmFyYXNpbW1hbjoxMjM0NTY4OQ==' } })
+            .then(response => {
+                console.log(response, history);
+                setMessageHandler({ ...MessageHandler, message: response.data.data.message, status: true });
+                handleClick();
+            })
+            .catch(error => {
+                console.log(error, error.response, error.message, error.request);
+                // setErrorStatus(error.response.data);
+                setMessageHandler({ ...MessageHandler, message: error.response.data, status: false });
+                handleClick();
+            })
     }
 
     const signUpHandler = () => {
@@ -45,7 +80,7 @@ function Login() {
                 pathname: '/signUp',
             }
         );
-    } 
+    }
 
     const toggleModal = () => {
         setisModalOpen(!isModalOpen);
@@ -70,7 +105,7 @@ function Login() {
                 <div className="row">
                     <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>
                     <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center">
-                        <br /><br /><br /><br /><br /><br /><br />
+                        <br /><br /><br /><br /><br />
                         <div className="login">
                             <h5 style={{ color: "#B99E01" }}><i className="fa fa-graduation-cap"></i>Institute-<span style={{ color: "black" }}>Web</span></h5>
                             <h6 style={{ color: "#B99E01" }}>Log<span style={{ color: "black" }}>in</span></h6><br />
@@ -78,16 +113,16 @@ function Login() {
                                 <Button onClick={getOtpHandler} disabled={MobileNumber.length != 10} style={{ fontSize: "60%", float: "right" }} variant="contained" color="primary">Send Otp</Button><br /><div style={{ marginBottom: "12px" }} />
                                 <input placeholder="MobileNumber" type="number" value={MobileNumber} onChange={e => {
                                     setMobileNumber(e.target.value);
-                                    setErrorStatus(null);
+                                    // setErrorStatus(null);
                                 }} />
                                 <br /><div style={{ marginTop: "2px" }} />
                                 <input placeholder="OTP" type="number" value={OTP} onChange={e => {
                                     setOTP(e.target.value);
-                                    setErrorStatus(null);
+                                    // setErrorStatus(null);
                                 }} /><br /><br />
                                 <button onClick={mySubmitHandler} className="button button1" type="button" disabled={MobileNumber === '' || MobileNumber.length != 10 || OTP === ''}>Login</button><br /><br />
-                                {ErrorStatus ? <p style={{ color: "red" }}>{ErrorStatus}</p> : null}
-                                <span><Button onClick={signUpHandler} style={{ fontSize: "60%", float: "right" }} variant="contained" color="primary">New User ?</Button></span><br/><br/>
+                                {/* {ErrorStatus ? <p style={{ color: "red" }}>{ErrorStatus}</p> : null} */}
+                                <span><Button onClick={signUpHandler} style={{ fontSize: "60%", float: "right" }} variant="contained" color="primary">New User ?</Button></span><br /><br />
                                 <span>
                                     <p style={{ fontSize: "80%", color: "#B99E01" }}>Institute-Web v1.0<br />
                                         <span style={{ color: "black" }}>Institute-Web Limited© 2020</span></p>
@@ -130,6 +165,20 @@ function Login() {
                     <Button style={{ fontSize: "70%" }} variant="contained" color="secondary" onClick={toggleModal}>Cancel</Button>
                 </ModalFooter>
             </Modal>
+            <Snackbar anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+            }} open={open} autoHideDuration={3000} onClose={handleClose}>
+                {
+                MessageHandler.status == true ?
+                    <Alert onClose={handleClose} severity="success">
+                       {MessageHandler.message}
+                    </Alert> :
+                    <Alert onClose={handleClose} severity="error">
+                        {MessageHandler.message}
+                    </Alert>
+                }
+            </Snackbar>
         </div>
     )
 }
